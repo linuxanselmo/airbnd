4b7af5ac5e00d753c8cb19b115f393927a3a7b55
Antonio Anselmo <programadorantonio@gmail.com>
2020-05-10 11:44:47 -0300

    finalizado o consumo da api, iniciando filtros

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 const url = "https://api.sheety.co/30b6e400-9023-4a15-8e6c-16aa4e3b1e72";
-let quartos = [];
+let dadosbrutos = [];
 let tipos = [];
 let totalPaginas = 1;
 
 function capturaDados(){
-    //criando a caputra dos dados e um try catch para proteção de erros
-
-        fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            quartos = data.map(
-                item => {
-                    if (!tipos.includes(item.property_type)){
-                        tipos.push(item.property_type);
-                    }
-                    item.priceFormatado = formatarPreco(item.price);
-                    return item;
-                });
-        })
-        .catch (err => {
-        })
-    }
+//criando a caputra dos dados e um try catch para proteção de erros
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        dadosbrutos = data.map(
+            item => {
+                if (!tipos.includes(item.property_type)){
+                    tipos.push(item.property_type);
+                }
+                item.priceFormatado = formatarPreco(item.price);
+                return item;
+            });
+            criarQuartos();
+    })
+    .catch (err => {
+    })
+}
 
 
 function formatarPreco(valor){
@@ -32,7 +32,11 @@ function formatarPreco(valor){
 }
  capturaDados();
 
- let dados = [...quartos];
+
+ console.log(tipos);
+ console.log(dadosbrutos);
+ console.log(dadosbrutos.priceFormatado);
+
 // carregandos os tipos de hospedagens no select
  $('#tipo').click(function(){
     var tipoLocal = [...tipos];
@@ -44,7 +48,23 @@ function formatarPreco(valor){
     $('#tipo').html(options).show;
  });
 
-    console.log(tipos);
-    console.log(quartos);
-    console.log(dados.priceFormatado);
+ function criarQuartos(){
+    const conteudoQuartos = document.getElementById('criarQuartos');
+    conteudoQuartos.innerHTML = '';
+
+    dadosbrutos.map(quarto=> {
+      const quartoHtml = `
+      <div class="quarto">
+        <figure>
+          <img src="${quarto.photo}" alt="${quarto.name}">
+        </figure>
+        <label class="txtTipo">${quarto.property_type}</label>
+        <p class="txtNome">${quarto.name}</p>
+        <p class="txtPreco"><strong>R$ ${quarto.priceFormatado}</strong>/noite</p>
+      </div>
+      `;
+      conteudoQuartos.insertAdjacentHTML('beforeend', quartoHtml);
+    });
+
+ }
 
